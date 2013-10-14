7c8baac963adc1833d957313e0be5a788d180139
Sean Carpenter <sean@seancarpenter.net>
2013-10-14 16:36:44 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cposc::Application.config.secret_token = 'eb84d631799f3e47255491e28e59a0e76825a5fb52e80d15729a5f79d4dca4b3f2d595f9c02de9bc5bc7135a607b7272bc46922fda75edc54b8e987ff00833fe'
